656b9d28cd6dba62c58aeaf286df4350b8417c84
David Fernandez Esteban <dfes@Enterprise.fritz.box>
2022-09-18 16:31:56 +0200

    Filewatcher

--- src/main/java/de/naivetardis/landscaper/configuration/FileWatcherConfig.java
+++ src/main/java/de/naivetardis/landscaper/configuration/FileWatcherConfig.java
@@ -2,8 +2,10 @@ package de.naivetardis.landscaper.configuration;
 
 import de.naivetardis.landscaper.jobs.FileDefinitionListener;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.devtools.filewatch.FileSystemWatcher;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.annotation.PreDestroy;
@@ -15,12 +17,17 @@ public class FileWatcherConfig {
 
     @Value("${definition.path}")
     private String definitionPath;
+    private final FileDefinitionListener fileDefinitionListener;
 
+    public FileWatcherConfig(FileDefinitionListener fileDefinitionListener) {
+        this.fileDefinitionListener = fileDefinitionListener;
+    }
 
+    @Bean
     public FileSystemWatcher fileSystemWatcher() {
         FileSystemWatcher fileSystemWatcher = new FileSystemWatcher();
         fileSystemWatcher.addSourceDirectory(new File(definitionPath));
-        fileSystemWatcher.addListener(new FileDefinitionListener());
+        fileSystemWatcher.addListener(fileDefinitionListener);
         fileSystemWatcher.start();
         log.info("Started watcher on {}", definitionPath);
         return fileSystemWatcher;
